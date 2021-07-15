5ba66460fb1bd717c368e98c32783af987b17c0f
Breitner Enrique Gonzalez Angarita <breinergonza@hotmail.com>
2021-07-14 23:34:03 -0500

    CleanCode

--- src/OrderVisitor.java
+++ src/OrderVisitor.java
@@ -4,45 +4,30 @@ import java.util.*;
 
 class OrderVisitor implements VisitorInterface {
 
-  private List<List<Object>> orders;
-
-  private Vector orderObjList;
   private double orderTotal;
-
   private CompositeOrder or;
 
   public OrderVisitor() {
-    orderObjList = new Vector();
-    orders = new ArrayList<List<Object>>();
     or = new CompositeOrder();
   }
 
   public void visit(NonCaliforniaOrder inp_order) {
-
     orderTotal = inp_order.getOrderAmount();
     or.addOrder(Arrays.asList("Non California", inp_order.getOrderAmount(), 0.0, 0.0, orderTotal));
-
-    //orderTotal = orderTotal + inp_order.getOrderAmount();
   }
 
   public void visit(CaliforniaOrder inp_order) {
-    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalTax();
     orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalTax();
-
     or.addOrder(Arrays.asList("California", inp_order.getOrderAmount(), inp_order.getAdditionalTax(), 0.0, orderTotal));
   }
 
   public void visit(OverseasOrder inp_order) {
-    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
     orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalSH();
-
     or.addOrder(Arrays.asList("Overseas", inp_order.getOrderAmount(), 0.0, inp_order.getAdditionalSH(), orderTotal));
   }
 
   public void visit(ColombianOrder inp_order) {
-    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
     orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalSH();
-
     or.addOrder(Arrays.asList("Colombian", inp_order.getOrderAmount(), 0.0, inp_order.getAdditionalSH(),  orderTotal));
   }
 

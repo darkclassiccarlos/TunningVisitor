9790513b8ffdbe97df9da071506b80d892fbae4d
Breitner Enrique Gonzalez Angarita <breinergonza@hotmail.com>
2021-07-14 22:19:27 -0500

    Composite pattern

--- src/OrderVisitor.java
+++ src/OrderVisitor.java
@@ -3,27 +3,47 @@ import com.sun.jndi.toolkit.dir.SearchFilter;
 import java.util.*;
 
 class OrderVisitor implements VisitorInterface {
+
+  private List<List<Object>> orders;
+
   private Vector orderObjList;
   private double orderTotal;
 
+  private CompositeOrder or;
+
   public OrderVisitor() {
     orderObjList = new Vector();
+    orders = new ArrayList<List<Object>>();
+    or = new CompositeOrder();
   }
 
   public void visit(NonCaliforniaOrder inp_order) {
-    orderTotal = orderTotal + inp_order.getOrderAmount();
+
+    orderTotal = inp_order.getOrderAmount();
+    or.addOrder(Arrays.asList("Non California", inp_order.getOrderAmount(), 0.0, 0.0, orderTotal));
+
+    //orderTotal = orderTotal + inp_order.getOrderAmount();
   }
 
   public void visit(CaliforniaOrder inp_order) {
-    orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalTax();
+    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalTax();
+    orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalTax();
+
+    or.addOrder(Arrays.asList("California", inp_order.getOrderAmount(), inp_order.getAdditionalTax(), 0.0, orderTotal));
   }
 
   public void visit(OverseasOrder inp_order) {
-    orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+    orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+
+    or.addOrder(Arrays.asList("Overseas", inp_order.getOrderAmount(), 0.0, inp_order.getAdditionalSH(), orderTotal));
   }
 
   public void visit(ColombianOrder inp_order) {
-    orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+    //orderTotal = orderTotal + inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+    orderTotal = inp_order.getOrderAmount() + inp_order.getAdditionalSH();
+
+    or.addOrder(Arrays.asList("Colombian", inp_order.getOrderAmount(), 0.0, inp_order.getAdditionalSH(),  orderTotal));
   }
 
   public void visit(CompositeOrder inp_order) {
@@ -34,5 +54,9 @@ class OrderVisitor implements VisitorInterface {
     return orderTotal;
   }
 
+  public List<List<Object>> getOrders() {
+    return or.getOrders();
+  }
 
 }
+

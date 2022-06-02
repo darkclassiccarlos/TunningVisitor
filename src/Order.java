7c845e834f9cde9eeb532ef1f62a608c0934696b
Carlos Bautista <cbautista@ifxcorp.com>
2022-06-01 20:20:07 -0500

    actualizando metodos en el uml estructural

--- src/Order.java
+++ src/Order.java
@@ -5,12 +5,4 @@ public interface Order {
 
   public  void accept(OrderVisitor v);
 
-
-/*  public void addOrder(Order or);
-
-  public Order getOrder(int position);
-
-  public abstract List<List<Object>> getOrders();
-
-  public void accept(OrderVisitor v); */
 }
